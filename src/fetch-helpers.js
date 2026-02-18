3f0ba0ce39b13a07b8ec3d654e5eef448d142582
Jonathan B <johnanber@gmail.com>
2026-02-18 13:10:59 -0500

    Update fetch-helpers.js
    Added error handling in the try block, and removed console.logs from the function

--- src/fetch-helpers.js
+++ src/fetch-helpers.js
@@ -3,11 +3,12 @@ export const getRandomCards = async () => {
     const response = await fetch(
       `https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0&sort=random&cachebust`
     );
+     if (!response.ok) {
+      throw new Error(`Failed to fetch card details`);
+    }
     const info = await response.json();
-    console.log(info.data);
     return { data: info.data, error: null };
   } catch (error) {
-    console.error(error);
     return { data: null, error: error };
   }
 };
