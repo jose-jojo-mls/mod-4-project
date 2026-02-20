db72da82287a7995a47321675384e989b82ba905
Jonathan <JohnAnBer@gmail.com>
2026-02-20 11:48:40 -0500

    i messed up

--- src/dom-helpers.js
+++ src/dom-helpers.js
@@ -1,9 +1,3 @@
-export const renderRandomCards = (data) => {
-  const li = document.createElement("li");
-  const img = document.createElement("img");
-  const h3 = document.createElement("h3");
-  const p = document.createElement("p");
-  const list = document.getElementById(cards - list);
 export const renderRandomCards = async (data) => {
   const list = document.querySelector(`#cards-list`);
   list.innerHTML = "";
@@ -26,18 +20,18 @@ export const renderRandomCards = async (data) => {
 };
 
 export const renderSingleCard = (card) => {
-    const cardTitle = document.querySelector('#card-title');
-    const cardDescription = document.querySelector('#card-description');
-    const cardImg = document.querySelector('#card-img');
-    const cardAtk = document.querySelector('#card-atk');
-    const cardDef = document.querySelector('#card-def');
-    const cardStars = document.querySelector('#card-stars');
+  const cardTitle = document.querySelector("#card-title");
+  const cardDescription = document.querySelector("#card-description");
+  const cardImg = document.querySelector("#card-img");
+  const cardAtk = document.querySelector("#card-atk");
+  const cardDef = document.querySelector("#card-def");
+  const cardStars = document.querySelector("#card-stars");
 
-    cardTitle.textContent = card.name;
-    cardDescription.textContent = card.desc;
-    cardImg.src = card.card_images[0].image_url_small;
-    cardImg.alt = card.name;
-    cardAtk.textContent = `${card.atk}`;
-    cardDef.textContent = `${card.def}`;
-    cardStars.textContent = `${card.level}`;
-}
+  cardTitle.textContent = card.name;
+  cardDescription.textContent = card.desc;
+  cardImg.src = card.card_images[0].image_url_small;
+  cardImg.alt = card.name;
+  cardAtk.textContent = `${card.atk}`;
+  cardDef.textContent = `${card.def}`;
+  cardStars.textContent = `${card.level}`;
+};
